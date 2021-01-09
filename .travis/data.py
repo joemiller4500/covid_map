04c0afc172832b5af2d1436cc975436f6501096d
joemiller <joemiller4500@gmail.com>
2021-01-08 23:37:56 -0800

    moved data.py to correct directory

--- .travis/data.py
+++ .travis/data.py
@@ -8,6 +8,8 @@ tsConfUS = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-1
 tsConfUS['FIPS'] = tsConfUS['FIPS'].fillna(0)
 # pad the fips code with leading zero if necessary
 tsConfUS['FIPS'] = tsConfUS['FIPS'].astype(int).astype(str).str.pad(width = 5, side='left', fillchar='0')
+latlongs = tsConfUS[['FIPS','Lat','Long_']]
+print(latlongs)
 
 # import census data
 censusTot = pd.read_csv('https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/counties/totals/co-est2019-alldata.csv', encoding = "ISO-8859-1")
@@ -17,7 +19,13 @@ censusTot['FIPS'] = censusTot['STATE'].astype(str).str.pad(width = 2, side='left
 censusTot = censusTot[['FIPS', 'POPESTIMATE2019']]
 
 voteCount = pd.read_csv('countypres_2000-2016.csv')
-print(voteCount)
+voteCount = voteCount[(voteCount['year'] == 2016)]
+# print(voteCount)
+voteCount = voteCount[(voteCount['candidate'] == 'Donald Trump')]
+voteCount['FIPS'] = voteCount['FIPS'].fillna(0)
+voteCount['FIPS'] = voteCount['FIPS'].astype(int).astype(str).str.pad(width = 5, side='left', fillchar='0')
+voteCount = voteCount[['FIPS', 'candidatevotes', 'totalvotes']]
+# print(voteCount)
 
 # read in shapefile with GeoPandas and rename FIPS key
 usgeo = gpd.read_file("geo_data/cb_2014_us_county_5m.shp")
@@ -27,13 +35,18 @@ usgeo['FIPS'] = usgeo['GEOID']
 # Dataframes
 usgeo = usgeo.merge(tsConfUS, on = 'FIPS')
 usgeo = usgeo.merge(censusTot, on = 'FIPS')
+voteCount = voteCount.merge(latlongs,how='left', on = 'FIPS').dropna()
+print(voteCount)
+
 
 # calculate fields to be used in JavaScript map
 usgeo['last'] = usgeo.iloc[:,-2]
-usgeo['rate'] = (usgeo['last'] - usgeo.iloc[:,-8])/(7*(usgeo['POPESTIMATE2019']))
+usgeo['rate'] = (usgeo['last'] - usgeo.iloc[:,-31])/(30*(usgeo['POPESTIMATE2019']))
 usgeo['rate'] = usgeo['rate'].round(decimals = 6)
-usgeo = usgeo[['NAME','FIPS','geometry','rate','last', 'POPESTIMATE2019','Lat','Long_']]
+usgeo['new'] = (usgeo['last'] - usgeo.iloc[:,-8])
+usgeo = usgeo[['NAME','FIPS','geometry','rate','last', 'POPESTIMATE2019','Lat','Long_', 'new']]
 usgeo['perc'] = (usgeo['last']/usgeo['POPESTIMATE2019']).round(decimals = 4)
 
 # use GeoPandas to export the dataframe to a GeoJSON file
 usgeo.to_file("usgeo.geojson", driver='GeoJSON')
+voteCount.to_csv("voteCount.csv")
